5fde51d45b696a2bed28c5208f6c3e759f881b9b
ColtonYork <cy925323@ohio.edu>
2024-09-17 20:57:07 -0400

    Add files via upload

---
+++ word-count.cc
@@ -0,0 +1,25 @@
+/**
+ *   @file: word-count.cc
+ * @author: Enter your name
+ *   @date: Enter the date
+ *  @brief: Add Description
+ */
+
+#include<iomanip>
+#include<cstdlib>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+
+cout << "hello" << endl; 
+
+
+
+
+    return 0;
+}
+
+
